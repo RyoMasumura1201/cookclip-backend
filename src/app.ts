55a67257b634c37b6f97f7b4b1ed821de6b66e07
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-03-01 07:41:18 +0900

    json parserを追加

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import userController from './controllers/userController';
 
 const app = express();
 
+app.use('/', express.json());
+
 app.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('hello express\n');
