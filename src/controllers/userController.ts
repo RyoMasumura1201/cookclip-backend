8e2669011d06090d76b119956d1bfdfcc8ca6149
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-03-07 08:30:18 +0900

    ユーザー全取得を削除

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,11 +4,6 @@ import { Router, Request, Response } from 'express';
 const prisma = new PrismaClient();
 const router = Router();
 
-router.get('/', async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany();
-  res.json({ users });
-});
-
 router.post('/', async (req: Request, res: Response) => {
   const { name, email } = req.body;
   const user = await prisma.user.upsert({
