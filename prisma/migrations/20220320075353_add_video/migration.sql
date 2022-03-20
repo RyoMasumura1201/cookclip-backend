cca91ee4ed1b425feba2c40859695a7384ebb389
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-03-20 16:54:24 +0900

    make video table

---
+++ prisma/migrations/20220320075353_add_video/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Video" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "videoId" TEXT NOT NULL,
+
+    CONSTRAINT "Video_pkey" PRIMARY KEY ("id")
+);
