a66946bd733d89fce554d8ec8f0a52ae7b67c4ec
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-03-22 07:58:34 +0900

    動画登録できるように

--- src/lib/registerVideo.ts
+++ src/lib/registerVideo.ts
@@ -30,38 +30,34 @@ type SearchMovieResult = {
   items: YoutubeMovie[];
 };
 
-const main = async () => {
-  // const searchResult = await axios.get<never, SearchMovieResult>(
-  //   'https://www.googleapis.com/youtube/v3/search',
-  //   {
-  //     params: {
-  //       part: 'snippet',
-  //       channelId: process.env.CHANNEL_ID_OF_RYUJI,
-  //       key: process.env.YOUTUBE_API_KEY,
-  //     },
-  //   },
-  // );
+type videoForPrisma = {
+  title: string;
+  videoId: string;
+  description: string;
+};
 
+const main = async () => {
   const existVideos = await prisma.video.findMany();
   const existVideoIdList = existVideos.map((video) => video.videoId);
 
-  const url =
-    'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=' +
-    process.env.CHANNEL_ID_OF_RYUJI +
-    '&key=' +
-    process.env.YOUTUBE_API_KEY +
-    '&maxResults=50' +
-    '&type=video';
+  const url = 'https://www.googleapis.com/youtube/v3/search';
+  const videoListForPrisma: videoForPrisma[] = [];
+  let pageToken = '';
+  while (true) {
+    try {
+      const res: AxiosResponse<SearchMovieResult> = await axios.get(url, {
+        params: {
+          part: 'snippet',
+          channelId: process.env.CHANNEL_ID_OF_RYUJI,
+          key: process.env.YOUTUBE_API_KEY,
+          maxResults: 50,
+          type: 'video',
+          pageToken: pageToken,
+        },
+      });
 
-  axios
-    .get(url)
-    .then(async (res: AxiosResponse<SearchMovieResult>) => {
       const videoList = res.data.items;
-      console.log('videoList');
-      console.log(videoList);
-
-      const videoListForPrisma = videoList
-        .filter((video) => video.id.videoId)
+      videoList
         .filter((video) => {
           return !existVideoIdList.includes(video.id.videoId);
         })
@@ -71,20 +67,31 @@ const main = async () => {
             title: video.snippet.title,
             description: video.snippet.description,
           };
+        })
+        .forEach((video) => {
+          videoListForPrisma.push(video);
         });
-
-      console.log('videoListForPrisma');
-      console.log(videoListForPrisma);
-
-      const result = await prisma.video.createMany({
-        data: videoListForPrisma,
-      });
-      console.log('result');
-      console.log(result);
-    })
-    .catch((e) => {
+      console.log(videoListForPrisma.length);
+      console.log(res.data.nextPageToken);
+      pageToken = res.data.nextPageToken;
+      if (!pageToken) {
+        break;
+      }
+    } catch (e) {
       console.log(e);
-    });
+      break;
+    }
+  }
+
+  const uniqueVideos = videoListForPrisma.filter((video, i, self) => {
+    return self.findIndex((v) => v.videoId === video.videoId) === i;
+  });
+
+  const result = await prisma.video.createMany({
+    data: uniqueVideos,
+  });
+  console.log('result');
+  console.log(result);
 };
 
 main();
